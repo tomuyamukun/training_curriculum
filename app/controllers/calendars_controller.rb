b8b1e071f0564cdf183233a3a85fd2b6e7cba2b3
tomuyamukun <73642660+tomuyamukun@users.noreply.github.com>
2020-11-11 17:34:08 +0900

    Revert "Revert "issue4~6の解消""

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    #planに変えるとDBには保存できる(入力されたものはplanクラスにvalitationされている)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -33,10 +34,9 @@ class CalendarsController < ApplicationController
       today_plans = []
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      en
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, wday: wdays[(@todays_date + x).wday], plans: today_plans}
       @week_days.push(days)
     end
-
   end
 end
